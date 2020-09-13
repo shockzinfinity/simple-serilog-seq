478312265f1a79bbcb6c838f539dc7365b3d514e
Jun Yu <shockz@shockz.io>
2020-09-13 13:11:47 +0900

    plug in serilog

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 
 namespace example
 {
@@ -7,14 +9,32 @@ namespace example
   {
     public static void Main(string[] args)
     {
-      CreateHostBuilder(args).Build().Run();
+      Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .WriteTo.Console()
+        .CreateLogger();
+
+      try
+      {
+        Log.Information("Starting up...");
+        CreateHostBuilder(args).Build().Run();
+      }
+      catch (Exception ex)
+      {
+        Log.Fatal(ex, "Application start-up failed.");
+      }
+      finally
+      {
+        Log.CloseAndFlush();
+      }
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
+      Host.CreateDefaultBuilder(args)
+          .UseSerilog()
+          .ConfigureWebHostDefaults(webBuilder =>
+          {
+            webBuilder.UseStartup<Startup>();
+          });
   }
 }
