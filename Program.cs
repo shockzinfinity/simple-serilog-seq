b62d902c8fe7d2edc9229c888a5e5b4cc135c12b
Jun Yu <shockz@shockz.io>
2020-09-13 17:06:38 +0900

    logger configuration

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
 using Serilog.Formatting.Compact;
 using System;
+using System.IO;
 
 namespace example
 {
   public class Program
   {
+    public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+      .SetBasePath(Directory.GetCurrentDirectory())
+      .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+      .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+      .AddEnvironmentVariables()
+      .Build();
+
     public static void Main(string[] args)
     {
       Log.Logger = new LoggerConfiguration()
@@ -19,11 +28,12 @@ namespace example
         .WriteTo.File(
           new RenderedCompactJsonFormatter(),
           "./logs/log.json",
-          rollingInterval: RollingInterval.Day, 
+          rollingInterval: RollingInterval.Day,
           rollOnFileSizeLimit: true, // maximum 1GB, could be null
           retainedFileCountLimit: 31) // number of files, could be null
-        //.WriteTo.Seq("http://localhost:5341")
-        .WriteTo.Seq(Environment.GetEnvironmentVariable("SEQ_URL") ?? "http://localhost:5341")
+          //.WriteTo.Seq("http://localhost:5341")
+          //.WriteTo.Seq(Environment.GetEnvironmentVariable("SEQ_URL") ?? "http://localhost:5341", apiKey: "l5ElRnJOpatCcDUjnAoI")
+        .WriteTo.Seq(Configuration.GetValue<string>("SEQ_URL"), apiKey: Configuration.GetValue<string>("SEQ_API_KEY"))
         .CreateLogger();
 
       try
